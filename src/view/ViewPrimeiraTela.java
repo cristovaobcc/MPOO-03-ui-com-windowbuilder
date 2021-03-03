8bd18201090dd66540b1dbfbdb769b912b9ff7bb
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-03 19:39:53 -0300

    Alterando o look and feel
    Fonte: https://www.youtube.com/watch?v=o0_LPAQYf7s&list=RDCMUCgOu28f2-cdegVHuZZhLDdA&index=3

--- src/view/ViewPrimeiraTela.java
+++ src/view/ViewPrimeiraTela.java
@@ -23,6 +23,24 @@ public class ViewPrimeiraTela extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+
+		/* Set the Nimbus look and feel */
+		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+		/* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+		 * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+		 */
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException | 
+				IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+			System.err.println(ex);
+		} 
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -45,23 +63,23 @@ public class ViewPrimeiraTela extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnAo = new JButton("Ação");
+
+		JButton btnAo = new JButton("Mostrar");
 		btnAo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				JOptionPane.showMessageDialog(null, "Nome:" + textField.getText()); 
-				
+
 			}
 		});
 		btnAo.setBounds(157, 176, 117, 25);
 		contentPane.add(btnAo);
-		
+
 		textField = new JTextField();
-		textField.setBounds(100, 80, 233, 19);
+		textField.setBounds(100, 80, 233, 25);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNome = new JLabel("Nome");
 		lblNome.setBounds(100, 53, 70, 15);
 		contentPane.add(lblNome);
